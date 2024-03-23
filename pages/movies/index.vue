b79eb29b8ac25694f7c89ff9ea7a5a2949916762
Omollo Oganda <cliffosego2000@gmail.com>
2024-03-24 00:02:10 +0300

    making http requests using the  function

--- pages/movies/index.vue
+++ pages/movies/index.vue
@@ -1,9 +1,27 @@
-<script lang="ts" setup></script>
+<script  setup>
+
+
+
+const query = ref("")
+const movies = ref([])
+async function search (){
+    const {Search} = await $fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=6c94eee8&s=${query.value}`)
+    movies.value = Search
+}
+</script>
 
 <template>
-  <div>
-    Page: movies
-  </div>
+  <form @submit.prevent="search">
+    <input type="text" v-model="query">
+    <button >Search</button>
+  </form>
+  <ul style="display: flex; flex-wrap: wrap; gap: 10px; list-style: none;">
+    <li v-for="movie in movies" :key="movie.imdbID">
+        <NuxtLink :to="{name: 'movies-id', params:{id: movie.imdbID}}">
+            <img :src="movie.Poster" :alt="movie.Title"
+        </NuxtLink>
+    </li>
+  </ul>
 </template>
 
 <style scoped></style>
