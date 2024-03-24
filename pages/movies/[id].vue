659ba522b6791000b2ca870d71929419048ed4a5
Omollo Oganda <cliffosego2000@gmail.com>
2024-03-24 12:36:07 +0300

    call useFetch for a   + useAsyncData shorthand

--- pages/movies/[id].vue
+++ pages/movies/[id].vue
@@ -1,17 +1,21 @@
 <script setup>
 const route = useRoute()
-const {data} = useAsyncData(`/movies/${route.params.id}`,() => {
-   return $fetch(`http://www.omdbapi.com/?&apikey=6c94eee8&i=${route.params.id}`)
-}, {
-    pick: ["Plot", "Title"]
-    // transform(data){
-    //     return {
-    //         Plot: data.Plot,
-    //         Title: data.Title
-    //     }
-    //      return data.Title
-    // }
+const {data} = useFetch(`http://www.omdbapi.com/?&apikey=6c94eee8&i=${route.params.id}`, {
+    pick: ["Plot", "Title"],
+    key: `/movies/${route.params.id}`
 })
+// const {data} = useAsyncData(`/movies/${route.params.id}`,() => {
+//    return $fetch(`http://www.omdbapi.com/?&apikey=6c94eee8&i=${route.params.id}`)
+// }, {
+//     pick: ["Plot", "Title"]
+//     // transform(data){
+//     //     return {
+//     //         Plot: data.Plot,
+//     //         Title: data.Title
+//     //     }
+//     //      return data.Title
+//     // }
+// })
 
 
 </script>
