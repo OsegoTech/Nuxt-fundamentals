76703e70ad7ac27f1a929cbb1d1791474d703531
Omollo Oganda <cliffosego2000@gmail.com>
2024-03-24 12:50:58 +0300

    handle useFetch errors

--- pages/movies/[id].vue
+++ pages/movies/[id].vue
@@ -1,9 +1,23 @@
 <script setup>
 const route = useRoute()
-const {data} = useFetch(`http://www.omdbapi.com/?&apikey=6c94eee8&i=${route.params.id}`, {
-    pick: ["Plot", "Title"],
-    key: `/movies/${route.params.id}`
+const { data, error } = await useFetch(`http://www.omdbapi.com/?&apikey=6c94eee8&i=${route.params.id}`, {
+    pick: ["Plot", "Title", "Error"],
+    key: `/movies/${route.params.id}`,
+    // onResponse({request, response}){
+    //     if(response._data.Error === "Incorrect IMDb ID."){
+    //         showError({statusCode: 404, statusMessage: "Page Not Found"})
+    //     }
+    // },
+    // onResponseError (){
+    //     showError({statusCode: 500, statusMessage: "Ooh noes!"})
+    // }
 })
+if (error.value){
+    // handle accordingly
+}
+if (data.value.Error === "Incorrect IMDb ID.") {
+    showError({ statusCode: 404, statusMessage: "Page Not Found" })
+}
 // const {data} = useAsyncData(`/movies/${route.params.id}`,() => {
 //    return $fetch(`http://www.omdbapi.com/?&apikey=6c94eee8&i=${route.params.id}`)
 // }, {
